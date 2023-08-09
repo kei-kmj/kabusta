cbe200b34a35c77e4066d3ff36396cd96a21907f
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2023-08-09 14:27:07 +0900

    first commit

--- kabusapi_token.py
+++ kabusapi_token.py
@@ -2,24 +2,28 @@ import urllib.request
 import json
 import pprint
 
-obj = { 'APIPassword': 'testtest' }
-json_data = json.dumps(obj).encode('utf8')
+class TokenManager:
+    @staticmethod
+    def fetch_token():
+        obj = { 'APIPassword': 'testtest' }
+        json_data = json.dumps(obj).encode('utf8')
 
-url = 'http://localhost:18081/kabusapi/token'
-req = urllib.request.Request(url, json_data, method='POST')
-req.add_header('Content-Type', 'application/json')
+        url = 'http://localhost:18081/kabusapi/token'
+        req = urllib.request.Request(url, json_data, method='POST')
+        req.add_header('Content-Type', 'application/json')
 
-try:
-    with urllib.request.urlopen(req) as res:
-        print(res.status, res.reason)
-        for header in res.getheaders():
-            print(header)
-        print()
-        content = json.loads(res.read())
-        pprint.pprint(content)
-except urllib.error.HTTPError as e:
-    print(e)
-    content = json.loads(e.read())
-    pprint.pprint(content)
-except Exception as e:
-    print(e)
+        try:
+            with urllib.request.urlopen(req) as res:
+                content = json.loads(res.read())
+                return content['Token']
+        except urllib.error.HTTPError as e:
+            content = json.loads(e.read())
+            print(content)
+        except Exception as e:
+            print(e)
+
+        return None
+
+
+if __name__ == '__main__':
+    print(TokenManager.fetch_token())
